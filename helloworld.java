1aadef88f67095b0b01654efdcf751f345334d66
SMARTCSE <47376568+SMARTCSE@users.noreply.github.com>
2019-03-08 02:38:36 -0800

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+Class Ideone
+//Main
+{
+public class HelloWorld {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World");
+		}
+	}
+ }
+}
